bef98ac7878a92bec18ec206d4cb8e65965ab586
Kristler <kristler@dmginc.gg>
2023-06-11 20:30:13 +0100

    Added structure of future stats cards API

--- src/partials/Home/StatCards.tsx
+++ src/partials/Home/StatCards.tsx
@@ -1,23 +1,42 @@
 import StatCard from "../../components/StatCard/StatCard";
 
+interface IStatsProps {
+  status: string;
+  last_check: string;
+  message: string;
+  server_count: number;
+  total_games: number;
+  total_users: number;
+}
+
 export default function StatCards() {
+
+  const dummyStats: IStatsProps = {
+    status: "operational",
+    last_check: "2023-06-11 12:52:22",
+    message: "bot is connected v2",
+    server_count: 582,
+    total_games: 6547,
+    total_users: 5625
+  }
+
   return (
     <div className="relative w-full z-30">
       <div className="flex -translate-y-24 flex-row flex-wrap z-30 w-full align-middle justify-center gap-9">
         <StatCard
-          count={600}
+          count={dummyStats.server_count}
           label="Servers"
           aosIndex={0}
         />
 
         <StatCard
-          count={8000}
-          label="Users"
+          count={dummyStats.total_users}
+          label="Active Players"
           aosIndex={1}
         />
 
         <StatCard
-          count={80000}
+          count={dummyStats.total_games}
           label="Matches Played"
           aosIndex={2}
         />
